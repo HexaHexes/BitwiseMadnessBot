b1b4e3d5e0cef056925fc88faf4f664297e81812
Tiago Ribeiro <tiagodribeiro@hotmail.com>
2020-04-30 20:35:21 +0100

    Implemented Client

--- bitwise-madness-client/build.gradle.kts
+++ bitwise-madness-client/build.gradle.kts
@@ -1,1 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+dependencies {
+    implementation("org.javacord:javacord:3.0.5")
+    implementation("io.ktor:ktor-client:1.3.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.0-RC2")
+
+}
